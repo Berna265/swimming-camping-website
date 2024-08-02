8219e9c20fef22d2a82bb716f2a872c3574f253a
Berna265 <152644616+Berna265@users.noreply.github.com>
2024-08-02 16:15:19 +0200

    initial commit

---
+++ resources/views/dash.blade.php
@@ -0,0 +1,4 @@
+<h1>dashboard</h1>
+<a href="/home">visit homepage</a>
+
+<p>{{ $name }}</p>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        Logo
+                    </div>
+                    <div class="col">
+                        Links
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- Style the section tag height -->
+    <section>
+        <div> 
+            <img>
+            <div>
+                <!-- Search bar -->
+                 <input type="text" placeholder="Search" name="query">
+                 <button>
+                    Search
+                 </button>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
